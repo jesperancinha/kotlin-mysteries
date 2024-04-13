40ce95a74547998ee7920ff4ad26406b8b1e383c
jesperancinha <jofisaes@gmail.com>
2024-04-13 14:44:02 +0200

    Swagger definition complete

---
+++ carparts-manager/Readme.md
@@ -0,0 +1,5 @@
+# CarParts Manager
+
+## Swagger location
+
+- [http://localhost:8080/api/v1/carparts/swagger-ui/index.html](http://localhost:8080/api/v1/carparts/swagger-ui/index.html)
--- carparts-manager/build.gradle.kts
+++ carparts-manager/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
