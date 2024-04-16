13a142db354c49d143d3f4cae3c5c65d09dc0c3a
jesperancinha <jofisaes@gmail.com>
2024-04-16 22:28:49 +0200

    The non-nullables yet nullables

---
+++ carparts-null-play/src/main/kotlin/CarPartDto.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.talks.carparts
+
+import java.math.BigDecimal
+import java.time.Instant
+
+data class CarPartDto(
+    val id: Long,
+    val name: String,
+    val productionDate: Instant,
+    val expiryDate: Instant,
+    val barCode: Long,
+    val cost: BigDecimal
+)
--- carparts-null-play/src/main/kotlin/Main.kt
+++ carparts-null-play/src/main/kotlin/Main.kt
@@ -1,9 +1,34 @@
 package org.jesperancinha.talks.carparts
-fun main() {
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        println("i = $i")
-    }
+import java.lang.reflect.Field
+import java.math.BigDecimal
+import java.time.Instant
+
+fun main() {
+    val carPartDto = CarPartDto(
+        id = 123L,
+        name = "name",
+        productionDate = Instant.now(),
+        expiryDate = Instant.now(),
+        cost = BigDecimal.TEN,
+        barCode = 1234L
+    )
+    println(carPartDto)
+    val field: Field = CarPartDto::class.java
+        .getDeclaredField("name")
+    field.isAccessible = true
+    field.set(carPartDto, null)
+    println(carPartDto)
+    assert(carPartDto.name == null)
+    println(carPartDto.name == null)
+
+//    carPartDto.name = null
+//    val carPartDto2 = CarPartDto(
+//        id = 123L,
+//        name = null,
+//        productionDate = Instant.now(),
+//        expiryDate = Instant.now(),
+//        cost = BigDecimal.TEN,
+//        barCode = 1234L
+//    )
 }
