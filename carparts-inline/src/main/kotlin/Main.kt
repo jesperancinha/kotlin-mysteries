a0d110c7c7f48b59943a8d19856ac3e2e4086838
jesperancinha <jofisaes@gmail.com>
2024-04-17 09:37:01 +0200

    cross inline and labels

--- carparts-inline/src/main/kotlin/Main.kt
+++ carparts-inline/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.jesperancinha.talks.carparts
 
+import java.time.LocalDateTime
+
 fun main() {
     callEngine()
     callACopyOfAnEngine(123)
@@ -10,6 +12,39 @@ fun main() {
     cleanGlass {
         println("Glass cleaned!")
     }
+    callEngineCrossInline {
+        println("Place key in ignition")
+        println("Turn key or press pus button ignition")
+        println("Clutch to the floor")
+        println("Set the first gear")
+        true
+    }.run { println(this) }
+}
+
+inline fun callEngineCrossInline(crossinline startManually: () -> Boolean): Boolean {
+    run loop@{
+        for (i in 1..100) {
+            (1..10).forEach { subI ->
+                println("Just one time! $i - $subI")
+                return@loop;
+            }
+        }
+    }
+    val introductionResult = run loop@{
+        val test = introduction {
+            println("Get computer in the backseat")
+            return@introduction startManually()
+        }
+        test
+    }
+    println("Engine started!")
+    return introductionResult
+}
+
+inline fun introduction(intro: () -> Boolean): Boolean {
+    println(LocalDateTime.now())
+    intro()
+    return true
 }
 
 fun callEngine() {
@@ -21,6 +56,6 @@ inline fun <reified T> callACopyOfAnEngine(value: T) {
     println(value)
 }
 
-inline fun cleanGlass(f: () -> Unit){
+inline fun cleanGlass(f: () -> Unit) {
     f()
 }
