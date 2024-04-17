ccb1cc56e6094bc77d0966f36767086da83ff13b
jesperancinha <jofisaes@gmail.com>
2024-04-17 22:25:18 +0200

    tailrec example

--- carparts-tairec/src/main/kotlin/Main.kt
+++ carparts-tairec/src/main/kotlin/Main.kt
@@ -1,16 +1,92 @@
 package org.jesperancinha.talks.carparts
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+sealed interface Part {
+    val totalWeight: Double
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+sealed interface ComplexPart  : Part{
+    val parts: List<Part>
+}
+data class CarPart(val name: String, val weight: Double) : Part {
+    override val totalWeight: Double
+        get() = weight
+}
+
+data class ComplexCarPart(
+    val name: String,
+    val weight: Double,
+    override val parts: List<Part>
+) :
+    ComplexPart {
+    override val totalWeight: Double
+        get() = weight
+}
+
+data class Car(
+    val name: String,
+    override val parts: List<Part>
+) : ComplexPart {
+    override val totalWeight: Double
+        get() = parts.sumOf { it.totalWeight }
+}
+
+tailrec fun totalWeight(parts: List<Part>, acc: Double = 0.0): Double {
+    if (parts.isEmpty()) {
+        return acc
     }
+    val part = parts.first()
+    val remainingParts = parts.drop(1)
+    val currentWeight = acc + part.totalWeight
+    return when (part) {
+        is ComplexPart -> totalWeight(remainingParts + part.parts, currentWeight)
+        else -> totalWeight(remainingParts, currentWeight)
+    }
+}
+
+fun main() {
+    val cars =
+        listOf(
+            Car(
+                "Anna", listOf(
+                    CarPart("Chassis", 50.0),
+                    CarPart("Engine", 100.0),
+                    CarPart("Transmission", 150.0),
+                    ComplexCarPart(
+                        "Frame", 500.0,
+                        listOf(
+                            CarPart("Screw", 1.0),
+                            CarPart("Screw", 2.0),
+                            CarPart("Screw", 3.0),
+                            CarPart("Screw", 4.0),
+                        )
+                    ),
+                    CarPart("Suspension", 200.0),
+                    CarPart("Wheels", 100.0),
+                    CarPart("Seats", 50.0),
+                    CarPart("Dashboard", 30.0),
+                    CarPart("Airbags", 20.0)
+                )
+            ),
+            Car(
+                "George", listOf(
+                    ComplexCarPart(
+                        "Chassis", 300.0,
+                        listOf(
+                            CarPart("Screw", 1.0),
+                            CarPart("Screw", 2.0),
+                            CarPart("Screw", 3.0),
+                            CarPart("Screw", 4.0),
+                        )
+                    ),
+                    CarPart("Engine", 300.0),
+                    CarPart("Transmission", 150.0),
+                    CarPart("Seats", 50.0),
+                    CarPart("Dashboard", 30.0),
+                    CarPart("Airbags", 20.0)
+                )
+            )
+        )
+
+    val total = totalWeight(cars)
+    println("Total weight of ${cars.size} is $total kg")
 }
