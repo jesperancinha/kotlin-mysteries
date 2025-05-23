6f2786caa2e5a54e81af01a24a5fff19b6a1e45e
jesperancinha <jofisaes@gmail.com>
2025-05-23 09:51:24 +0200

    monitoring the possibility of setting a null value in a non-nullable variable in Kotlin

---
+++ carparts-null-play/src/test/kotlin/org/jesperancinha/talks/carparts/MainTest.kt
@@ -0,0 +1,11 @@
+package org.jesperancinha.talks.carparts
+
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun `should monitor null value for different versions with reflection directly`() {
+        main()
+    }
+
+}
