10bb0349eeaad3c33d069f2854d4c19a66cd4980
João Esperancinha <jofisaes@gmail.com>
2025-05-27 09:56:36 +0200

    Adds unnamed classes test

---
+++ carparts-java-play/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "org.jesperancinha.vtcc"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("--enable-preview")
+}
+
+tasks.withType<Test>().configureEach {
+    jvmArgs = listOf("--enable-preview")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs = listOf("--enable-preview")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "Main"
+        )
+    }
+}
+
+application {
+    mainClass.set("")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 rootProject.name = "kotlin-mysteries"
-include ("carparts-manager")
-include ("carparts-null-play")
+include("carparts-manager")
+include("carparts-null-play")
 include("carparts-inline")
 include("carparts-tailrec")
 include("carparts-data-structures")
 include("carparts-use-site-targets")
+include("carparts-java-play")
