697d8147950c43487405f9593abb95dc5dc7aabe
jesperancinha <jofisaes@gmail.com>
2026-07-20 11:47:23 +0200

    Kotlin updates

---
+++ Prompts.md
@@ -0,0 +1,7 @@
+# Kotlin Mysteries Prompts
+
+## 1. Finding out why unit tests aren't being triggered
+
+```text
+when invoking ./gradlew test from the command line, the tests aren't being run. can you find out why?
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
+
+subprojects {
+    tasks.withType<Test> {
+        testLogging {
+            events("passed", "skipped", "failed")
+            showStandardStreams = true
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
+    }
+}
